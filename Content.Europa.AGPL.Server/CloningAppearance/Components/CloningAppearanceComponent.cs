ee6f924360db7b50406046fb515bf0749e358afc
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-07-27 16:56:35 +0300

    [Port] Cloning Appearance System (#13)
    Co-authored-by: Rxup <backmen@bk.ru>

---
+++ Content.Europa.AGPL.Server/CloningAppearance/Components/CloningAppearanceComponent.cs
@@ -0,0 +1,15 @@
+﻿using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+
+namespace Content.Europa.AGPL.Server.CloningAppearance.Components;
+
+[RegisterComponent]
+public sealed partial class CloningAppearanceComponent : Component
+{
+    [DataField("components")]
+    [AlwaysPushInheritance]
+    public ComponentRegistry Components { get; private set; } = new();
+
+    [DataField("startingGear")]
+    public ProtoId<StartingGearPrototype>? Gear;
+}
