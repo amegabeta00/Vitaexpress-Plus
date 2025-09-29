d335719c4dd837b654f434af7a332d2336d9ff7f
ILLUZORR <92508353+ILLUZORR@users.noreply.github.com>
2025-09-29 06:16:45 +0500

    Hz

--- Content.Server/Ghost/GhostSystem.cs
+++ Content.Server/Ghost/GhostSystem.cs
@@ -185,7 +185,6 @@ namespace Content.Server.Ghost
 
         private EntityQuery<GhostComponent> _ghostQuery;
         private EntityQuery<PhysicsComponent> _physicsQuery;
-
         private static readonly ProtoId<TagPrototype> AllowGhostShownByEventTag = "AllowGhostShownByEvent";
         private static readonly ProtoId<DamageTypePrototype> AsphyxiationDamageType = "Asphyxiation";
         private static readonly ProtoId<DamageTypePrototype> IonDamageType = "Ion";
