92389ce657c6adcde954dbcd2ae18fd2b1b05f1f
ILLUZORR <92508353+ILLUZORR@users.noreply.github.com>
2025-10-16 09:37:52 +0500

    Shit

--- Content.Server.Database/Migrations/Postgres/20251016041354_RoleWhitelist.cs
+++ Content.Server.Database/Migrations/Postgres/20251016041354_RoleWhitelist.cs
@@ -9,129 +9,67 @@ namespace Content.Server.Database.Migrations.Postgres
     /// <inheritdoc />
     public partial class RoleWhitelist : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_role_whitelists_player_player_user_id",
-                table: "role_whitelists");
+    migrationBuilder.CreateTable(
+        name: "role_whitelist",
+        columns: table => new
+        {
+            role_whitelist_id = table.Column<int>(type: "integer", nullable: false)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+            player_id = table.Column<Guid>(type: "uuid", nullable: false),
+            first_time_added = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+            first_time_added_by = table.Column<Guid>(type: "uuid", nullable: false),
+            how_many_times_added = table.Column<int>(type: "integer", nullable: false),
+            in_whitelist = table.Column<bool>(type: "boolean", nullable: false),
+            last_time_added = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+            last_time_added_by = table.Column<Guid>(type: "uuid", nullable: false),
+            last_time_removed = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+            last_time_removed_by = table.Column<Guid>(type: "uuid", nullable: true)
+        },
+        constraints: table =>
+        {
+            table.PrimaryKey("PK_role_whitelist", x => x.role_whitelist_id);
+            table.ForeignKey(
+                name: "FK_role_whitelist_player_player_id",
+                column: x => x.player_id,
+                principalTable: "player",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Cascade);
+        });
 
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_role_whitelists",
-                table: "role_whitelists");
+    migrationBuilder.CreateTable(
+        name: "role_whitelist_log",
+        columns: table => new
+        {
+            role_whitelist_log_id = table.Column<int>(type: "integer", nullable: false)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+            admin_id = table.Column<Guid>(type: "uuid", nullable: false),
+            player_id = table.Column<Guid>(type: "uuid", nullable: false),
+            role_whitelist_action = table.Column<string>(type: "text", nullable: false),
+            time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+        },
+        constraints: table =>
+        {
+            table.PrimaryKey("PK_role_whitelist_log", x => x.role_whitelist_log_id);
+        });
 
-            migrationBuilder.DropColumn(
-                name: "role_id",
-                table: "role_whitelists");
+    migrationBuilder.CreateIndex(
+        name: "IX_role_whitelist_player_id",
+        table: "role_whitelist",
+        column: "player_id",
+        unique: true);
 
-            migrationBuilder.RenameTable(
-                name: "role_whitelists",
-                newName: "role_whitelist");
+    migrationBuilder.CreateIndex(
+        name: "IX_role_whitelist_log_admin_id",
+        table: "role_whitelist_log",
+        column: "admin_id");
 
-            migrationBuilder.RenameColumn(
-                name: "player_user_id",
-                table: "role_whitelist",
-                newName: "player_id");
-
-            migrationBuilder.AddColumn<int>(
-                name: "role_whitelist_id",
-                table: "role_whitelist",
-                type: "integer",
-                nullable: false,
-                defaultValue: 0)
-                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "first_time_added",
-                table: "role_whitelist",
-                type: "timestamp with time zone",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "first_time_added_by",
-                table: "role_whitelist",
-                type: "uuid",
-                nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
-
-            migrationBuilder.AddColumn<int>(
-                name: "how_many_times_added",
-                table: "role_whitelist",
-                type: "integer",
-                nullable: false,
-                defaultValue: 0);
-
-            migrationBuilder.AddColumn<bool>(
-                name: "in_whitelist",
-                table: "role_whitelist",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "last_time_added",
-                table: "role_whitelist",
-                type: "timestamp with time zone",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "last_time_added_by",
-                table: "role_whitelist",
-                type: "uuid",
-                nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "last_time_removed",
-                table: "role_whitelist",
-                type: "timestamp with time zone",
-                nullable: true);
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "last_time_removed_by",
-                table: "role_whitelist",
-                type: "uuid",
-                nullable: true);
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_role_whitelist",
-                table: "role_whitelist",
-                column: "role_whitelist_id");
-
-            migrationBuilder.CreateTable(
-                name: "role_whitelist_log",
-                columns: table => new
-                {
-                    role_whitelist_log_id = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    admin_id = table.Column<Guid>(type: "uuid", nullable: false),
-                    player_id = table.Column<Guid>(type: "uuid", nullable: false),
-                    role_whitelist_action = table.Column<string>(type: "text", nullable: false),
-                    time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_role_whitelist_log", x => x.role_whitelist_log_id);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_role_whitelist_player_id",
-                table: "role_whitelist",
-                column: "player_id",
-                unique: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_role_whitelist_log_admin_id",
-                table: "role_whitelist_log",
-                column: "admin_id");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_role_whitelist_log_player_id",
-                table: "role_whitelist_log",
-                column: "player_id");
-        }
+    migrationBuilder.CreateIndex(
+        name: "IX_role_whitelist_log_player_id",
+        table: "role_whitelist_log",
+        column: "player_id");
+}
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
